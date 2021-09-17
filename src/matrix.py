ac723de7930bcb4775ec7f1c3cfc8a95243f8cb3
SameerRao22 <43189259+SameerRao22@users.noreply.github.com>
2021-09-17 13:23:19 -0700

    Fixed rref. Added fancy print

--- src/matrix.py
+++ src/matrix.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import copy
 import matplotlib.pyplot as plt
+import math
 
 #scales a matrix M by a factor S
 def scale(S, M):
@@ -79,30 +80,92 @@ def det(M):
     return D
 
 def mPrint(M):
+    for j in M:
+        s = ""
+        for k in j:
+            s += str(k).ljust(10)
+        print(s)
+    print()
+
+'''
+Pretty Print Function:
+Changes brackets based on row numbers
+Colors diagonals
+Formats values
+'''
+def pPrint(M):
+    CRED = '\033[91m' #Header for red text
+    CEND = '\033[0m'  #Footer for text
+
+    mid = []  #Finds middle rows
+    if (len(M)) % 2 == 0:
+        even = True
+        mid.append(len(M)/2-1)
+        mid.append(len(M)/2)
+    else:
+        even = False
+        a = math.floor((len(M))/2)
+        mid.append(a)
+
     for i in range(len(M)):
-        if i == 0:
-            s = "⎡"
+
+        if i == 0:  #Chooses bracket based on row number
+            s = "⎧".ljust(3)
         elif i == len(M)-1:
-            s = "⎣"
-        else:
-            s = "⎜"
-        for j in range(len(M[i])):
-            if M[i][j] == 0:
-                M[i][j] = abs(M[i][j])
-            M[i][j] = "{:.3f}".format(M[i][j])
-            if (j != len(M[i]) - 1): 
-                s += str(M[i][j]).ljust(10) 
+            s = "⎩".ljust(3)
+        elif i in mid:
+            if even:
+                if i == mid[0]:
+                    s = "⎭".ljust(3)
+                else:
+                    s = "⎫".ljust(3)
             else:
-                s += str(M[i][j])
-        if i == 0:
-            s += "⎤"
-        elif i == len(M)-1:
-            s += "⎦"
+                s = "⎨".ljust(3)
         else:
-            s += "⎟"
+            s = "|".ljust(3)
+
+        for j in range(len(M[i])):
+            flag = False
+            if i == j:  #Identifies points on diagonals
+                flag = True
+
+            if M[i][j] == 0:
+                M[i][j] = abs(math.floor(M[i][j]))
+
+            if M[i][j] >= 0:  #Makes negative numbers shorter
+                M[i][j] = "{:.3f}".format(M[i][j])
+            else:
+                M[i][j] = "{:.2f}".format(M[i][j])
+
+            if (j != len(M[i]) - 1):  #Formats ends
+                if flag:  #Colors diagonal 
+                    s += CRED+str(M[i][j]).ljust(10)+CEND
+                else:
+                    s += str(M[i][j]).ljust(10)
+            else:
+                if flag:  #Colors diagonal 
+                    s += CRED+str(M[i][j]).ljust(7)+CEND
+                else:
+                    s += str(M[i][j]).ljust(7)
+
+        if i == 0:  #Ending bracket
+            s += "⎫"
+        elif i == len(M)-1:
+            s += "⎭"
+        elif i in mid:
+            if even:
+                if i == mid[0]:
+                    s += "⎩"
+                else:
+                    s += "⎧"
+            else:
+                s += "⎬"
+        else:
+            s += "|"
+
         print(s)
 
-#transforms a vector V using a matrix M
+#transforms a vector V using a matrix M and graphs it
 def transform(M, V):
   plt.style.use('dark_background')
   plt.plot(V[0],V[1],'magenta')
@@ -110,19 +173,23 @@ def transform(M, V):
   plt.plot(V2[0],V2[1],'cyan')
   plt.show()
 
+#swaps two rows
 def rowSwap(M, r1, r2):
     v = M[r1]
     M[r1] = M[r2]
     M[r2] = v
 
+#scales a row by a vector
 def rowScale(M, r1, s):
     for i in range(len(M[0])):
         M[r1][i] *= s
 
+#sums 2 rows with a scalar
 def rowSum(M, r1, r2, s):
     for i in range(len(M[0])):
         M[r2][i] += s*M[r1][i]
 
+#identifies rows with all zeros
 def zeros(M):
     z = []
     for i in range(len(M)):
@@ -134,9 +201,11 @@ def zeros(M):
             z.append(i)
     return z
 
+#reduces a matrix to reduced row echelon form
 def rref(M):
     M = copy.deepcopy(M)
-    allZero = True
+
+    allZero = True  #checks if a matrix is all zero
     for i in range(len(M)):
         for j in range(len(M[0])):
                 if M[i][j] != 0:
@@ -144,30 +213,39 @@ def rref(M):
                     break
     if allZero:
         return M
+    
+    #makes left corner non-zero
     i = 1
     while (M[0][0] == 0):
        rowSwap(M, 0, i)
        i+=1
-   
+    
+    #makes left corner one
     rowScale(M, 0, 1./M[0][0])
     
+    #chooses loop length for a rectangular matrix
     v = len(M)
     if len(M) > len(M[0]):
         v = len(M[0])
 
-    for r in range(v):
+    z = zeros(M)
+    z.sort(reverse=True)
+    last = len(M)
+    for r in range(v): 
+        if r in z:
+            last -= 1
+            rowSwap(M, r, last)
         for i in range(v):
             if (i == r):
                 continue
             if (M[r][r] != 0):
                 rowSum(M, r, i, (M[i][r]*-1)/(M[r][r]))
    
-    z = zeros(M)
-    z.sort(reverse=True)
-    last = len(M)
-    for i in range(len(z)):
-        last -= 1
-        rowSwap(M, z[i], last)
+
+    for r in range(v):
+        for i in range(v):
+            if r == i and M[r][i] != 0:
+                rowScale(M, r, 1.0/M[r][i])
     return M
 
 def identity(n):

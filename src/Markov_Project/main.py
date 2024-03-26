2be6d3211d282366a71d0f386906bb360b575229
Sameer Rao <sameerrao@El-MacBook-Air.local>
2024-03-26 13:54:24 -0700

    organizing

--- src/Markov_Project/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import os
-
-os.system('python3 src/generate.py')
-
-print('\n\n------Benchmark------')
-os.system('python3 src/benchmark.py')
-
-# os.system('python3 src/grammar.py data/corpus3.txt')
